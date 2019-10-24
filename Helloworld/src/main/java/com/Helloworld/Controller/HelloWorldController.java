ccfd4eccbb70b758f76d8184b2cf99d4b0905877
dorotheavincent01 <47728310+dorotheavincent01@users.noreply.github.com>
2019-10-24 17:47:19 +0800

    Tutorial 1

---
+++ Helloworld/src/main/java/com/Helloworld/Controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.Helloworld.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+@RequestMapping("/")
+@ResponseBody
+   // @GetMapping
+    public String HelloWorld(){
+
+        return "Hello World :) (:";
+    }
+
+
+}
