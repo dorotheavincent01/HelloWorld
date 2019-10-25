430b4e1b4b2efc096f83856339a66bb8eee6168a
dorotheavincent01 <47728310+dorotheavincent01@users.noreply.github.com>
2019-10-26 01:48:01 +0800

    Profiles & Logging

--- Helloworld/src/main/java/com/Helloworld/HelloWorldController.java
+++ Helloworld/src/main/java/com/Helloworld/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.Helloworld;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
+@ConfigurationProperties("spring")
+@Component
 public class HelloWorldController {
+
+    //@Value(value = "${spring.name}")
+     @Value("${spring.name}")
+    private String name;
+
     @RequestMapping("/helloWorld")
     @ResponseBody
+    @GetMapping
     public String index()
     {
-        return "Hello World :) (:";
+        if(name.equalsIgnoreCase("error")){
+            throw new RuntimeException("Opss error!!!");
+        }
+        return name;
+      //  return "Hello World :) (:";
     }
 }
